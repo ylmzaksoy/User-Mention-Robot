56ba96fb1ae4d758552ce3e5590baa120506c824
yk35140003 <88341444+yk35140003@users.noreply.github.com>
2021-08-30 07:04:44 +0300

    update bot.py

--- bot.py
+++ bot.py
@@ -75,7 +75,7 @@ async def mentionall(event):
     usrtxt = ""
     async for usr in client.iter_participants(event.chat_id):
       usrnum += 1
-      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}, ) "
+      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}), "
       if event.chat_id not in anlik_calisan:
         await event.respond("İşlem Başarılı Bir Şekilde Durduruldu ❌")
         return
